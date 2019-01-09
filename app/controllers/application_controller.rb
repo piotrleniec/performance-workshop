eca3f37d19d17e18e7ffb5bda03124ca33e0b6bf
Piotr Leniec <leniec.piotr@gmail.com>
2019-01-09 22:44:09 +0100

    add task2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,18 @@ class ApplicationController < ActionController::Base
 
   def task1_questions
   end
+
+  def task2
+    @rows = []
+    Room.first(1000).each do |room|
+      @rows << [
+        room.id,
+        room.prices.minimum(:amount),
+        room.prices.maximum(:amount)
+      ]
+    end
+  end
+
+  def task2_questions
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'application#index'
   get '/task1', to: 'application#task1'
   get '/task1_questions', to: 'application#task1_questions'
+  get '/task2', to: 'application#task2'
+  get '/task2_questions', to: 'application#task2_questions'
 end
