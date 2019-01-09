60945301287e24dba61128d4c85d178e70d8143a
Piotr Leniec <leniec.piotr@gmail.com>
2019-01-09 19:48:27 +0100

    add task1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  def index
+  end
+
+  def task1
+    @landlords = Landlord.first(10)
+  end
+
+  def task1_questions
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#index'
+  get '/task1', to: 'application#task1'
+  get '/task1_questions', to: 'application#task1_questions'
 end
